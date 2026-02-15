c6fa8cf3634713b394e2dbb2a58f441350c05931
vitoria-marca <vmslucia@inf.ufpel.edu.br>
2026-02-15 18:54:37 -0300

    setando formatos de data e nome do app

--- configuracao/settings.py
+++ configuracao/settings.py
@@ -123,13 +123,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
+USE_L10N = True
+
+DATE_INPUT_FORMATS = [
+    '%d/%m/%Y',  
+    '%d/%m/%y', 
+    '%Y-%m-%d', 
+]
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
